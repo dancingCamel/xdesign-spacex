d4504f050e515a3e851eb8b19410a2eaa1a7662a
dancingCamel <robert.brindley.morgan+github@gmail.com>
2020-12-08 18:20:22 -0500

    Added basic styling, api fetch, and loop over launches

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,134 @@
 import "./App.css";
 // build css from scss here
 // https://techcookbook.com/react/use-scss-with-create-react-app#:~:text=%20Use%20SCSS%20with%20create-react-app%20%201%20Building,contents%20into%20the%20body%20of%20the...%20More%20
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import Header from "./components/Header";
 import LaunchItem from "./components/LaunchItem";
+import Logo from "./assets/spacex-logo.png";
+import launchHome1x from "./assets/img/launch-home.png";
+import launchHome2x from "./assets/img/launch-home@2x.png";
+import launchHome3x from "./assets/img/launch-home@3x.png";
 
-const PageWrapper = styled.div`
+const AppWrapper = styled.div`
+    box-sizing: border-box;
+`;
+
+const Header = styled.header`
     display: flex;
     justify-content: between;
+    align-items: flex-end;
+    min-height: 8vh;
+    font-size: calc(10px + 1vmin);
+    color: white;
+    width: 100vw;
+    padding: 10px 5vmin 0px 5vmin;
 `;
+
+const ContentWrapper = styled.div`
+    // display: flex;
+    // flex-direction: row;
+    // justify-content: space-between;
+`;
+
+const LaunchWrapper = styled.section``;
 const Left = styled.div`
+    float: left;
     width: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `;
 const Right = styled.div`
-    width: 60%;
+    float: right;
+    width: 40%;
 `;
 
 const App = () => {
-    // hit api when page loads using hook
-    // call api when hit refresh button - save data to somewhere
+    const [launches, setLaunches] = useState([]);
 
-    // have function to sort return data using data from memory
-    // have another function that filters data by year / filter is a state that changes the api call - watch for it changing. should be a dropdown
+    const getAllLaunches = async () => {
+        const latestLaunches = await fetch(
+            "https://api.spacexdata.com/v3/launches"
+        );
+        const latestLaunchesJson = await latestLaunches.json();
+
+        sessionStorage.setItem(
+            "latestLaunches",
+            JSON.stringify(latestLaunchesJson)
+        );
+
+        setLaunches(latestLaunchesJson);
+    };
+
+    const sortLaunches = (direction) => {
+        // maybe have a state to track asc/desc, clicking button toggles it. then use effect to call this funciton
+        // direction = desc/asc
+        if (direction === "descending") {
+            setLaunches((launches) => {
+                // sort here and return sorted array
+            });
+        } else {
+            setLaunches((launches) => {
+                // sort here and return sorted array
+            });
+        }
+        // setLaunches = sorted list from current state of launches
+    };
+
+    const filterLaunches = (year) => {
+        // setLaunches = launches from session storage filtered by year
+        // {todos.filter((todo) => !todo.complete).length} left to do
+    };
+
+    useEffect(() => {
+        getAllLaunches();
+    }, []);
+
+    // call getAllLaunches when hit refresh button
 
     return (
-        <div className="App">
-            <header className="App-header">
-                <Header />
-            </header>
-            <body>
-                <PageWrapper>
-                    {/* page wrapper with flexbox justify-content-between */}
-                    <Left>
-                        {/* import image at 80 vh and vertically centered with flexbox */}
-                    </Left>
+        <AppWrapper>
+            <Header>
+                {/* flexbox header bar. justify-content between. align refresh button end */}
+                <div>
+                    {/* logo */}
+                    <img src={Logo} alt="Space X Logo" height="45px" />
+                    <span>LAUNCHES</span>
+                </div>
+                <div className="refreshBtn">{/* refresh button */}</div>
+            </Header>
 
-                    <Right>
-                        {/* buttons div with styled components. flexbox flex end*/}
-                        {/* loop over api return data and show on screen */}
-                    </Right>
-                </PageWrapper>
-            </body>
-        </div>
+            <main>
+                <ContentWrapper>
+                    {/* button go here. aligned on right */}
+                    {/* page wrapper with flexbox justify-content-between */}
+                    <LaunchWrapper>
+                        <Left>
+                            <img
+                                src={launchHome1x}
+                                alt="Photo of a launch"
+                                srcSet={`${launchHome1x} 300w, ${launchHome2x} 768w, ${launchHome3x} 1240w`}
+                                className="launchImage"
+                                width="100%"
+                            />
+                        </Left>
+
+                        <Right>
+                            {/* loop over api return data and show on screen */}
+                            {launches.map((launch) => (
+                                <LaunchItem
+                                    key={launch.mission_name}
+                                    id={launch.flight_number}
+                                    mission={launch.mission_name}
+                                    rocket={launch.rocket.rocket_name}
+                                    date={launch.launch_date_local}
+                                />
+                            ))}
+                        </Right>
+                    </LaunchWrapper>
+                </ContentWrapper>
+            </main>
+        </AppWrapper>
     );
 };
 
--- src/components/LaunchItem.jsx
+++ src/components/LaunchItem.jsx
@@ -1,7 +1,49 @@
 import React from "react";
+import styled from "styled-components";
 
-const LaunchItem = () => {
-    return <div></div>;
+const Box = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-radius: 10px;
+    box-shadow: 1px 2px 5px grey;
+    width: 100%;
+    margin: 10px;
+`;
+
+const Left = styled.div`
+    display: flex;
+    flex-direction: row;
+`;
+
+const Id = styled.div`
+    margin: 20px;
+`;
+
+const Mission = styled.div`
+    margin: 20px;
+`;
+
+const Right = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+const LaunchItem = ({ key, id, mission, rocket, date }) => {
+    return (
+        <div>
+            <Box>
+                <Left>
+                    <Id>#{id}</Id>
+                    <Mission>{mission}</Mission>
+                </Left>
+                <Right>
+                    <div className="dateTag">{date}</div>
+                    <div className="rocketName">{rocket}</div>
+                </Right>
+            </Box>
+        </div>
+    );
 };
 
 export default LaunchItem;
